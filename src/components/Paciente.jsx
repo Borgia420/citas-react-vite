c95066bc151accb0739c238f077e86d71c110a6b
Ezequiel <cuentalibresuport@gmail.com>
2023-09-09 18:24:01 -0300

    first commit

---
+++ src/components/Paciente.jsx
@@ -0,0 +1,65 @@
+import Swal from "sweetalert2";
+const Paciente = ({paciente, setPaciente, eliminarPaciente}) => {
+
+    const { nombre, propietario, email, fecha, sintomas, id } = paciente
+
+    const handleEliminar = () => {
+        Swal.fire({
+            title: 'Desea eliminar este paciente?',
+            text: "Esta accion no podra revertirse",
+            icon: 'error',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Si, Eliminar!',
+            cancelButtonText: "Cancelar"
+            }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire(
+                'Deleted!',
+                'Your file has been deleted.',
+                'success'
+                )
+                eliminarPaciente(id)
+            }
+            })
+
+        //if(respuesta) {
+            //eliminarPaciente(id)
+        //}
+    }
+
+    return (
+    <div className="mx-5 my-10 bg-white shadow-md px-5 py-10 rounded-md">
+        <p className="font-bold mb-3 text-gray-700 uppercase">Nombre: {""}
+            <span className="font-normal normal-case">{nombre}</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Propietario: {""}
+            <span className="font-normal normal-case">{propietario}</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Email: {""}
+            <span className="font-normal normal-case">{email}</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Fecha Alta: {""}
+            <span className="font-normal normal-case">{fecha}</span>
+        </p>
+        <p className="font-bold mb-3 text-gray-700 uppercase">Sintomas: {""}
+            <span className="font-normal normal-case">{sintomas}</span>
+        </p>
+            <div className="flex justify-between mt-10">
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-indigo-600 hover:bg-indigo-700 text-white rounded-md font-bold uppercase"
+                    onClick={() => setPaciente(paciente)}
+                >Editar</button>
+                <button
+                    type="button"
+                    className="py-2 px-10 bg-red-600 hover:bg-red-700 text-white rounded-md font-bold uppercase"
+                    onClick={handleEliminar}
+                >Eliminar</button>
+            </div>
+        </div>
+    )
+}
+
+export default Paciente
